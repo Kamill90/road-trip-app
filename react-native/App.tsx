29e41a324eedfb3392abf6a4acf717acbfac363a
Kamil Lewandowski <kamil.lewandowski@stxnext.pl>
2019-07-26 22:57:18 +0200

    formatOnSave

--- react-native/App.tsx
+++ react-native/App.tsx
@@ -1,32 +1,27 @@
-import React from 'react';
-import { Fragment } from 'react';
+import React, { Component } from "react";
+import { Fragment } from "react";
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
-  StatusBar,
-} from 'react-native';
-import Geolocation from 'react-native-geolocation-service';
+  StatusBar
+} from "react-native";
+import Geolocation from "react-native-geolocation-service";
 
-
-class App extends React.Component {
+class App extends Component {
   componentDidMount() {
-    // Instead of navigator.geolocation, just use Geolocation.
-    // if (hasLocationPermission) {
-        Geolocation.getCurrentPosition(
-            (position) => {
-                console.warn(position);
-            },
-            (error) => {
-                // See error code charts below.
-                console.warn(error.code, error.message);
-            },
-            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-        );
-    // }
-}
+    Geolocation.getCurrentPosition(
+      position => {
+        console.warn(position);
+      },
+      error => {
+        console.warn(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );
+  }
 
   render() {
     return (
@@ -35,15 +30,14 @@ class App extends React.Component {
       </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   }
-})
-
+});
 
 export default App;
