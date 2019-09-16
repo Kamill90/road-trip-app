7ef5536fceaaa144a9bb9f62eb225c48c84fdb15
Kamil Lewandowski <kamil.lewandowski@stxnext.pl>
2019-09-16 08:08:09 +0200

    navigation

--- react-native/src/App.tsx
+++ react-native/src/App.tsx
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
-import { ApolloProvider, compose, graphql, MutationFn } from "react-apollo";
+import { ApolloProvider } from "react-apollo";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
 import { client } from "./api/client";
 import HomeScreen from "./home/HomeScreen";
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  }
+});
+
+const AppContainer = createAppContainer(AppNavigator);
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <HomeScreen />
+        <AppContainer />
       </ApolloProvider>
     );
   }
